6b55093f696ec3533e80684e5882bcd26123b32b
Hasan Rafi <mr.k779@outlook.com>
2020-12-04 03:18:02 +0600

    implemented cli

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Joker666/goworkerpool
 
 go 1.15
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli v1.22.5
+)
--- main.go
+++ main.go
@@ -2,38 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/Joker666/goworkerpool/basic"
 	"github.com/Joker666/goworkerpool/model"
 	"github.com/Joker666/goworkerpool/worker"
 	"github.com/Joker666/goworkerpool/workerpool"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	robust()
-}
-
-func nonRobust() {
 	// Prepare the data
 	var allData []model.SimpleData
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 1000; i++ {
 		data := model.SimpleData{ID: i}
 		allData = append(allData, data)
 	}
-	fmt.Printf("Start processing all work \n")
 
-	// Process
-	basic.Work(allData)
-	worker.NotPooledWork(allData)
-	worker.PooledWork(allData)
-	worker.PooledWorkError(allData)
-}
-
-func robust() {
 	var allTask []*workerpool.Task
-	for i := 1; i <= 20; i++ {
+	for i := 1; i <= 100; i++ {
 		task := workerpool.NewTask(func(data interface{}) error {
 			taskID := data.(int)
 			time.Sleep(100 * time.Millisecond)
@@ -44,23 +34,86 @@ func robust() {
 	}
 
 	pool := workerpool.NewPool(allTask, 5)
-	go func() {
-		for {
-			taskID := rand.Intn(100) + 20
 
-			if taskID%7 == 0 {
-				pool.Stop()
-			}
+	app := &cli.App{
+		Name:  "goworkerpool",
+		Usage: "check different work loads with worker pool",
+		Action: func(c *cli.Context) error {
+			fmt.Println("You need more parameters")
+			return nil
+		},
+		Commands: []cli.Command{
+			{
+				Name:  "basic",
+				Usage: "run synchronously",
+				Action: func(c *cli.Context) error {
+					basic.Work(allData)
+					return nil
+				},
+			},
+			{
+				Name:  "notpooled",
+				Usage: "run without any pooling",
+				Action: func(c *cli.Context) error {
+					worker.NotPooledWork(allData)
+					return nil
+				},
+			},
+			{
+				Name:  "pooled",
+				Usage: "run with pooling",
+				Action: func(c *cli.Context) error {
+					worker.PooledWork(allData)
+					return nil
+				},
+			},
+			{
+				Name:  "poolederror",
+				Usage: "run with pooling that handles errors",
+				Action: func(c *cli.Context) error {
+					worker.PooledWorkError(allData)
+					return nil
+				},
+			},
+			{
+				Name:  "wpool",
+				Usage: "run robust worker pool",
+				Action: func(c *cli.Context) error {
+					pool.Run()
+					return nil
+				},
+			},
+			{
+				Name:  "wpoolbg",
+				Usage: "run robust worker pool in background",
+				Action: func(c *cli.Context) error {
+					go func() {
+						for {
+							taskID := rand.Intn(100) + 20
 
-			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
-			task := workerpool.NewTask(func(data interface{}) error {
-				taskID := data.(int)
-				time.Sleep(100 * time.Millisecond)
-				fmt.Printf("Task %d processed\n", taskID)
-				return nil
-			}, taskID)
-			pool.AddTask(task)
-		}
-	}()
-	pool.RunBackground()
+							if taskID%7 == 0 {
+								pool.Stop()
+							}
+
+							time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+							task := workerpool.NewTask(func(data interface{}) error {
+								taskID := data.(int)
+								time.Sleep(100 * time.Millisecond)
+								fmt.Printf("Task %d processed\n", taskID)
+								return nil
+							}, taskID)
+							pool.AddTask(task)
+						}
+					}()
+					pool.RunBackground()
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
